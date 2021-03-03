5ef261f37450fbb6e0bca1b66e94838dcc61fc33
solutionLi <momo821123@outlook.com>
2021-03-03 04:27:01 +0300

    Modify Model

--- app/models.py
+++ app/models.py
@@ -8,5 +8,7 @@ class SportsURL(models.Model):
    sports = models.CharField(max_length = 30, primary_key = True)
    url = models.CharField(max_length = 200)
    sheet = models.CharField(max_length = 20)
+   victory = models.IntegerField()
+   rule = models.CharField(max_length = 100)
    class Meta:
       db_table = "sportsurl"
