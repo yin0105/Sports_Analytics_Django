513903fb491251277577b91947bc4202b73c6f2f
solutionLi <momo821123@outlook.com>
2021-03-05 19:52:43 +0300

    Fix start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 start cmd /k "cd /d 'E:\Workspace\Django Projects\Freelancer.com\Sports_Analytics_Django' & env\Scripts\activate & python manage.py runserver"
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://127.0.0.1:8000/"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://127.0.0.1:8000/"
