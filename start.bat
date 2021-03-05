be426572da305867ac92368f814b93c7e0953a2e
solutionLi <momo821123@outlook.com>
2021-03-05 10:36:59 +0300

    Correct start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,3 @@
-e:
-cd "Workspace\Django Projects\Freelancer.com\Sports_Analytics_Django\env\scripts"
-activate
-cd ..
-cd ..
-python manage.py runserver
+@echo off
+start cmd /k "cd /d 'E:\Workspace\Django Projects\Freelancer.com\Sports_Analytics_Django' & env\Scripts\activate & python manage.py runserver"
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://127.0.0.1:8000/"
